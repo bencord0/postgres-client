0b141fe822250b45401294c6f318e4e80c11f236
Ben Cordero <bencord0@condi.me>
2024-02-22 21:38:05 +0000

    Move messages::backend::DataRow to it's own file

---
+++ src/messages/backend/data_row.rs
@@ -0,0 +1,62 @@
+use std::{
+    error::Error,
+    io::Read,
+    str,
+};
+use crate::readers::*;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct DataRow {
+    pub fields: Vec<Option<String>>,
+}
+
+impl DataRow {
+    pub fn read_next_message(stream: &mut impl Read) -> Result<Self, Box<dyn Error>> {
+        let field_count = read_u16(stream)? as usize;
+        let mut fields: Vec<Option<String>> = vec![None; field_count as usize];
+
+        for index in 0..field_count {
+            let field_length = read_u32(stream)? as usize;
+
+            match field_length {
+                0xFFFFFFFF => {
+                    continue;
+                }
+                size => {
+                    let field_value = read_bytes(size, stream)?;
+                    fields[index] = Some(str::from_utf8(&field_value)?.to_string());
+                }
+            }
+        }
+
+        Ok(DataRow { fields })
+    }
+}
+
+impl DataRow {
+    pub fn builder() -> DataRowBuilder {
+        DataRowBuilder { fields: Vec::new() }
+    }
+}
+
+pub struct DataRowBuilder {
+    fields: Vec<Option<String>>,
+}
+
+impl DataRowBuilder {
+    pub fn string_field(mut self, field: impl Into<String>) -> Self {
+        self.fields.push(Some(field.into()));
+        self
+    }
+
+    pub fn null_field(mut self) -> Self {
+        self.fields.push(None);
+        self
+    }
+
+    pub fn build(self) -> DataRow {
+        DataRow {
+            fields: self.fields,
+        }
+    }
+}
--- src/messages/backend/mod.rs
+++ src/messages/backend/mod.rs
@@ -8,8 +8,10 @@ use crate::{messages::Message, readers::*};
 
 mod ready_for_query;
 mod row_description;
+mod data_row;
 pub use ready_for_query::ReadyForQuery;
 pub use row_description::RowDescription;
+pub use data_row::DataRow;
 
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -20,63 +22,6 @@ pub enum BackendMessage {
     CommandComplete(CommandComplete),
     Error { length: u32 },
 }
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct DataRow {
-    pub fields: Vec<Option<String>>,
-}
-
-impl DataRow {
-    pub fn builder() -> DataRowBuilder {
-        DataRowBuilder { fields: Vec::new() }
-    }
-}
-
-pub struct DataRowBuilder {
-    fields: Vec<Option<String>>,
-}
-
-impl DataRowBuilder {
-    pub fn string_field(mut self, field: impl Into<String>) -> Self {
-        self.fields.push(Some(field.into()));
-        self
-    }
-
-    pub fn null_field(mut self) -> Self {
-        self.fields.push(None);
-        self
-    }
-
-    pub fn build(self) -> DataRow {
-        DataRow {
-            fields: self.fields,
-        }
-    }
-}
-
-impl DataRow {
-    pub fn read_next_message(stream: &mut impl Read) -> Result<Self, Box<dyn Error>> {
-        let field_count = read_u16(stream)? as usize;
-        let mut fields: Vec<Option<String>> = vec![None; field_count as usize];
-
-        for index in 0..field_count {
-            let field_length = read_u32(stream)? as usize;
-
-            match field_length {
-                0xFFFFFFFF => {
-                    continue;
-                }
-                size => {
-                    let field_value = read_bytes(size, stream)?;
-                    fields[index] = Some(str::from_utf8(&field_value)?.to_string());
-                }
-            }
-        }
-
-        Ok(DataRow { fields })
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct CommandComplete {
     pub tag: String,
